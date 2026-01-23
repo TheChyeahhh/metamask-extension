2ff76db1f2758a7516a4fe11af222d658bf00782
Tatiana Bobritsky <tatiana.bobritsky@consensys.net>
2026-01-22 20:14:32 -0500

    Use createSnapAccount flow in create-remove-account-snap spec

--- test/e2e/tests/account/create-remove-account-snap.spec.ts
+++ test/e2e/tests/account/create-remove-account-snap.spec.ts
@@ -6,8 +6,10 @@ import { withFixtures } from '../../helpers';
 import AccountListPage from '../../page-objects/pages/account-list-page';
 import HeaderNavbar from '../../page-objects/pages/header-navbar';
 import SnapListPage from '../../page-objects/pages/snap-list-page';
-import SnapSimpleKeyringPage from '../../page-objects/pages/snap-simple-keyring-page';
-import { installSnapSimpleKeyring } from '../../page-objects/flows/snap-simple-keyring.flow';
+import {
+  installSnapSimpleKeyring,
+  createSnapAccount,
+} from '../../page-objects/flows/snap-simple-keyring.flow';
 import { login } from '../../page-objects/flows/login.flow';
 import { mockSnapSimpleKeyringAndSite } from './snap-keyring-site-mocks';
 
@@ -27,8 +29,7 @@ describe('Create and remove Snap Account', function (this: Suite) {
       async ({ driver }: { driver: Driver }) => {
         await login(driver);
         await installSnapSimpleKeyring(driver);
-        const snapSimpleKeyringPage = new SnapSimpleKeyringPage(driver);
-        await snapSimpleKeyringPage.createNewAccount();
+        await createSnapAccount(driver);
 
         // Check snap account is displayed after adding the snap account.
         await driver.switchToWindowWithTitle(
